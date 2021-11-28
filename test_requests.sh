fd1ee6ba4a5fc5fe4609c6e497a2d229acc972aa
Yuriy Skakovskiy <uta123@yandex-team.ru>
2021-11-28 19:57:03 +0300

    readme

--- test_requests.sh
+++ test_requests.sh
@@ -1,6 +1,14 @@
-curl -X POST 'localhost:80/person' --data-raw '{"login":"uta6","first_name":"ura","last_name":"skakovskiy","age":23}' -v
-curl -X POST 'localhost:80/person' --data-raw '{"login":"uta7","first_name":"ura","last_name":"skakovskiy","age":23}' -v
-curl -X POST 'localhost:80/person' --data-raw '{"login":"uta8","first_name":"ura","last_name":"skakovskiy","age":23}' -v
-curl -X GET 'localhost:80/person?login=uta7' -v
-curl -X GET 'localhost:80/person?login=uta8' -v
-curl -X GET 'localhost:80/person?first_name=ur' -v
+#!/bin/bash
+
+curl -X POST 'localhost:80/person' --data-raw '{"login":"uta6","first_name":"ura","last_name":"skakovskiy","age":23}'
+echo ''
+curl -X POST 'localhost:80/person' --data-raw '{"login":"uta7","first_name":"ura","last_name":"skakovskiy","age":23}'
+echo ''
+curl -X POST 'localhost:80/person' --data-raw '{"login":"uta8","first_name":"ura","last_name":"skakovskiy","age":23}'
+echo ''
+curl -X GET 'localhost:80/person?login=uta7'
+echo ''
+curl -X GET 'localhost:80/person?login=uta8'
+echo ''
+curl -X GET 'localhost:80/person?first_name=ur'
+echo ''
